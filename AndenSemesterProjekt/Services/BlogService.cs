d2fc550ad1d47cab0242329bc10e52c9e98a3c8d
Simon Bau <simo87m5@edu.zealand.dk>
2023-05-02 13:25:59 +0200

    IblogService INterface implemented

--- AndenSemesterProjekt/Services/BlogService.cs
+++ AndenSemesterProjekt/Services/BlogService.cs
@@ -1,9 +1,43 @@
 ﻿using AndenSemesterProjekt.Interfaces;
+using AndenSemesterProjekt.Models;
 
 namespace AndenSemesterProjekt.Services
 {
     public class BlogService : IBlogService
     {
+        public Post CreateBlogPost(string information)
+        {
+            throw new NotImplementedException();
+        }
 
+        public void deleteBlogPost(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Post> GetAllBlogPosts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Post> GetAllBlogPostsByCategory(string category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Post> GetAllBlogPostsByCriteria(string criteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Post GetBlogPostById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBlogPost(int id, Post post)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
