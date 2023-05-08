4e282ee42f0a8692b6c4a34cd6af299ece7100a8
Simon Bau <simo87m5@edu.zealand.dk>
2023-05-08 13:45:10 +0200

    Lavede basic admin/mockadmin

---
+++ AndenSemesterProjekt/Models/Admin.cs
@@ -0,0 +1,18 @@
+﻿namespace AndenSemesterProjekt.Models
+{
+    public class Admin
+    {
+
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public Admin(string username, string password)
+        {
+            UserName = username;
+            Password = password;
+        }
+
+        public Admin() { }
+
+    }
+}
