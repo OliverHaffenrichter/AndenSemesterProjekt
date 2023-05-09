3b4e3fb469cc0a950fb88a2287dda2cdc6798311
AlexanderThoegersen <117358130+AlexanderThoegersen@users.noreply.github.com>
2023-05-09 11:37:22 +0200

    Richtext editor works somewhat

---
+++ AndenSemesterProjekt/Pages/Blog/CreateBlog.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AndenSemesterProjekt.Pages.Blog
+{
+    public class CreateBlogModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
