31b9584bfe71cabbf2ee867c5003d90b0d99e3c1
Simon Bau <simo87m5@edu.zealand.dk>
2023-05-09 14:58:52 +0200

    Smed en razerpage ind i Products (DisplayCategories)

---
+++ AndenSemesterProjekt/Pages/Products/DisplayCategories.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AndenSemesterProjekt.Pages.Products
+{
+    public class DisplayCategoriesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
